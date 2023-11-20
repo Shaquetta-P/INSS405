cad260739c56cc3b62036ea3fdb1a1cd683896e2
Shaquett-P/INSS405 <phillipss0907@students.bowiestate.edu>
2023-11-19 22:02:12 -0500

    Lab 4

---
+++ Lab4.py
@@ -0,0 +1,22 @@
+import math
+
+for a in range(11):
+ print(a)
+for b in range(1,11):
+    print(b)
+for c in range (1,10,2):
+    print(c)
+import math
+
+print(math.pi)
+Circumference= 20
+r=Circumference/2*math.pi
+print(r)
+float(input('Enter radius: ' ))
+A=math.pi*r**2
+print(A)
+
+Length=int(input('Enter Rectangle Length 12'))
+Width=int(input('Enter Rectangle Width 6'))
+Area=Length*Width
+print(Area)
